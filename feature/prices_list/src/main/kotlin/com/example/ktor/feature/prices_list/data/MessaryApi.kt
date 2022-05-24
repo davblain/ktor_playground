bb584d2c073048b121f7cc57ea49eb309d1c9a27
Александр Яцук <yatsuk@softinvent.ru>
2022-05-24 12:28:27 +0300

    * update dispatcher

--- feature/prices_list/src/main/kotlin/com/example/ktor/feature/prices_list/data/MessaryApi.kt
+++ feature/prices_list/src/main/kotlin/com/example/ktor/feature/prices_list/data/MessaryApi.kt
@@ -5,6 +5,8 @@ import com.example.ktor.core.network.safeRequest
 import com.example.ktor.feature.prices_list.data.models.PricesResponse
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object MessaryApiRoutes {
     private const val BASE_URL = "https://data.messari.io/api/v2"
@@ -14,10 +16,12 @@ object MessaryApiRoutes {
 internal class MessaryApi(private val client: HttpClient) {
 
     suspend fun getPrices(): ServerResponse<Unit, PricesResponse> {
-        return safeRequest {
-            client.get {
-                url(MessaryApiRoutes.ASSESTS)
-                parameter("fields", "id,slug,symbol,metrics/market_data/price_usd")
+        return withContext(Dispatchers.IO) {
+            safeRequest {
+                client.get {
+                    url(MessaryApiRoutes.ASSESTS)
+                    parameter("fields", "id,slug,symbol,metrics/market_data/price_usd")
+                }
             }
         }
     }
